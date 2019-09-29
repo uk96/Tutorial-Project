70c4d528d70639526b2eef0f8e2f23ef7f09f022
uk96 <uddeshkadu@gmail.com>
2019-09-29 11:44:57 +0530

    Api call working

---
+++ Mern-Stack-Tutorial-1/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://root:rootuser@tuorialuddesh-sjwno.mongodb.net/mern_shopping_list',
+    configMongoose: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
--- Mern-Stack-Tutorial-1/server.js
+++ Mern-Stack-Tutorial-1/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const config = require('./config/config');
+
+const items = require('./routes/api/items');
 
 const app = express();
 
+// BodyParser Middleware
 app.use(bodyParser.json());
+
+// DB Config
+const db = config.mongoURI;
+
+// Connect to mongoDB
+mongoose
+    .connect(db , config.configMongoose)
+    .then(() => console.log('MongoDB Connected....'))
+    .catch((err) => console.log(err));
+
+//Use Routes
+app.use('/api/items',items);
+
+const port = process.env.port || 5000;
+
+app.listen(port,() => console.log('Server started on port '+port));
+
 
